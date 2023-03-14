26f6b0c885c031bce69a2a6571221a00e13d493b
danhhoangtan <danhhoangtan.it@outlook.com>
2023-03-14 11:15:44 +0700

    commit wtf

--- QuanLyNhaThuoc/Controllers/HomeController.cs
+++ QuanLyNhaThuoc/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using QuanLyNhaThuoc.Models;
 
 namespace QuanLyNhaThuoc.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            var sanPhams = db.sanPhams.Include(s => s.LoaiSanPham).Include(s => s.NhaSanXuat);
+            return View(sanPhams.ToList());
         }
 
         public ActionResult About()
@@ -25,6 +32,22 @@ namespace QuanLyNhaThuoc.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = db.sanPhams
+                                .Where(item => item.MaSanPham == id)
+                                .Include(loai => loai.LoaiSanPham)
+                                .Include(sanXuat => sanXuat.NhaSanXuat).First();
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPham);
         }
     }
 }

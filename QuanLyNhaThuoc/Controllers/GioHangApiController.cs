779e7a7156c19a3eb4fc752817cebb2be6291c7b
danhhoangtan <danhhoangtan.it@outlook.com>
2023-03-31 10:01:44 +0700

    feature: gio hang

--- QuanLyNhaThuoc/Controllers/GioHangApiController.cs
+++ QuanLyNhaThuoc/Controllers/GioHangApiController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web.Http;
 using Antlr.Runtime;
 using Microsoft.AspNet.Identity;
+using QuanLyNhaThuoc.DTOs;
 using QuanLyNhaThuoc.Models;
 
 namespace QuanLyNhaThuoc.Controllers
@@ -17,56 +19,78 @@ namespace QuanLyNhaThuoc.Controllers
             _dbContext = new ApplicationDbContext();
         }
 
-        [HttpGet]        
-        public IHttpActionResult AddToCart(string productId, int amount)
+        [HttpPost]        
+        [Route("api/GioHangApi/AddToCart")]
+        public IHttpActionResult AddToCart(CartItemDto dto)
         {
             string userId = User.Identity.GetUserId();
-            var gioHang = _dbContext.gioHangs.FirstOrDefault(cart => cart.Id == userId);
-            if (gioHang == null)
+            
+            // insert or update ChiTietGioHang
+            var cart = _dbContext.gioHangs.FirstOrDefault(c => c.Id == userId);
+            if (cart == null)
             {
-                gioHang = new GioHang
+                cart = new GioHang
                 {
                     Id = userId,
                     NgayLap = DateTime.Now
                 };
-                _dbContext.gioHangs.Add(gioHang);
+                _dbContext.gioHangs.Add(cart);
             }
-            ChiTietGioHang cartDetail = new ChiTietGioHang
-            {
-                MaGiohang = gioHang.MaGioHang,
-                MaSanPham = productId,
-                SoLuong = amount
-            };
-            _dbContext.chiTietGioHangs.Add(cartDetail);
+
+            ChiTietGioHang cartDetail =
+                _dbContext.chiTietGioHangs.FirstOrDefault(cd =>
+                    cd.MaGiohang == cart.MaGioHang && cd.MaSanPham == dto.productId);
+            if (cartDetail == null)
+                cartDetail = new ChiTietGioHang
+                {
+                    MaGiohang = cart.MaGioHang,
+                    MaSanPham = dto.productId,
+                    SoLuong = dto.amount
+                };
+            else
+                cartDetail.SoLuong += dto.amount;
+            
+            _dbContext.chiTietGioHangs.AddOrUpdate(cartDetail);
+            
             _dbContext.SaveChanges();
-           return Ok();
+            return Ok();
         }
 
         [HttpPost]
-        public IHttpActionResult RemoveFromCart(string productId, int cartId)
+        [Route("api/GioHangApi/RemoveFromCart")]
+        public IHttpActionResult RemoveFromCart(CartItemDto dto)
         {
-            var cart = _dbContext.gioHangs.Where(c => c.MaGioHang == cartId).FirstOrDefault();
+            string userId = User.Identity.GetUserId();
+            var cart = _dbContext.gioHangs.FirstOrDefault(c => c.Id == userId);
             if (cart == null)
             {
                 return BadRequest();
             }
-            var cartDetail = _dbContext.chiTietGioHangs.Where(c => c.MaSanPham == productId && c.MaGiohang == cartId).FirstOrDefault();
+            var cartDetail = _dbContext.chiTietGioHangs.FirstOrDefault(c => c.MaSanPham == dto.productId && c.MaGiohang == cart.MaGioHang);
             _dbContext.chiTietGioHangs.Remove(cartDetail);
             _dbContext.SaveChanges();
             return Ok();
         }
 
         [HttpPost]
-        public IHttpActionResult UpdateCart(string productId, int amount, int cartId)
+        [Route("api/GioHangApi/UpdateCart")]
+        public IHttpActionResult UpdateCart(CartItemDto dto)
         {
-            var cart = _dbContext.gioHangs.Where(c => c.MaGioHang == cartId).FirstOrDefault();
+            string userId = User.Identity.GetUserId();
+            var cart = _dbContext.gioHangs.FirstOrDefault(c => c.Id == userId);
             if (cart == null)
             {
                 return BadRequest();
             }
-            var cartDetail = _dbContext.chiTietGioHangs.Where(c => c.MaSanPham == productId && c.MaGiohang == cartId).FirstOrDefault();
-            cartDetail.SoLuong = amount;
-            _dbContext.chiTietGioHangs.AddOrUpdate(cartDetail);
+            var cartDetail = _dbContext.chiTietGioHangs.FirstOrDefault(c => c.MaSanPham == dto.productId && c.MaGiohang == cart.MaGioHang);
+            if (cartDetail == null)
+                cartDetail.SoLuong = dto.amount;
+
+
+            if (dto.amount == 0)
+                _dbContext.chiTietGioHangs.Remove(cartDetail);
+            else 
+                _dbContext.chiTietGioHangs.AddOrUpdate(cartDetail);
             _dbContext.SaveChanges();
             return Ok();
         }
---
+++ QuanLyNhaThuoc/DTOs/CartItemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace QuanLyNhaThuoc.DTOs
+{
+    public class CartItemDto
+    {
+        public string productId { get; set; }
+        public int amount { get; set; }
+    }
+}
